90bbcf4d8f4d33826330d06e76134d51e2ac6916
Jaza K <jaza-k@protonmail.com>
2020-10-17 19:39:07 -0600

    define post entity

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,24 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType, Int } from "type-graphql";
 
+// class is both an ObjectType and Entity (decorators can be stacked)
+@ObjectType()
 @Entity()
 export class Post  {
 
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: 'date'})
-  createdAt = new Date();
+  createdAt = new Date(); 
 
+  @Field(() => String)
   @Property({ type:'date', onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field()
   @Property({ type: 'text' })
   title!: string;
 
