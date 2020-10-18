447b1150b556efcc0e522a4ee4b88c54e1fe764b
Jaza K <jaza-k@protonmail.com>
2020-10-17 19:38:29 -0600

    create express server + set up apollo

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    const post = orm.em.create(Post, {title: 'my first post'});
-    await orm.em.persistAndFlush(post);
+    const app = express();
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [PostResolver],
+            validate: false
+        }),
+        // special object that is accessible by all our objects
+        context: () => ({ em: orm.em }) // pass in em object
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("Express is listening on port 4000...")
+    });
 }
 
 main().catch((err) => {
     console.error(err);
-}); 
+});
