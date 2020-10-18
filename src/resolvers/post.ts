1703e957b28daf643c369d200929b12702072d2b
Jaza K <jaza-k@protonmail.com>
2020-10-17 19:40:38 -0600

    use type-graphql to interact with database

---
+++ src/resolvers/post.ts
@@ -0,0 +1,31 @@
+import { Post } from "../entities/Post";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
+import { MyContext } from "src/types";
+
+@Resolver()
+export class PostResolver {
+    @Query(() => [Post])
+    // return type is explicitly stated (Promise <Post[]>) to add type checking
+    posts(@Ctx() { em }: MyContext): Promise <Post[]> {
+        return em.find(Post, {});
+    }
+
+    @Query( () => Post, { nullable: true } )
+    // return type is explicitly stated (Promise <Post[]>) to add type checking
+    post(
+        @Arg("id") id: number, 
+        @Ctx() { em }: MyContext
+    ): Promise <Post | null> {
+        return em.findOne(Post, { id });
+    }
+
+    @Mutation( () => Post)
+    async createPost(
+        @Arg("title") title: string, 
+        @Ctx() { em }: MyContext
+    ): Promise <Post> {
+        const post = em.create(Post, {title})
+        await em.persistAndFlush(post)
+        return post;
+    }
+}
