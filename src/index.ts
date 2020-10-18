342a5e875f63e2f2864a8ae79bea93f1192cf5e2
Jaza K <jaza-k@protonmail.com>
2020-10-18 00:51:55 -0600

    create + add user resolver

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
+    await orm.getMigrator().up(); // automatically runs on server restart
 
     const app = express();
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [PostResolver],
+            resolvers: [PostResolver, UserResolver],
             validate: false
         }),
         // special object that is accessible by all our objects
